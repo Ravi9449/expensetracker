98902898a2b22d36e2911d3323a10345405472f1
Ravi9449 <Hanuman98@>
2023-03-15 23:58:15 +0100

    added voice recognisition for routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Speech from "./components/Speech/Speech";
 
 function App() {
   return (
     <div>
       <div>
         <Header />
+      </div>
+      <div>
+        <Speech />
       </div>
       <div>
         <Footer />
